535b6f427397afb965eb78d1201a9f44bf049ca0
furkan <kilic.furkank@gmail.com>
2024-12-30 18:14:46 +0300

    from docker

--- src/main/java/com/example/furkantest/HelloController.java
+++ src/main/java/com/example/furkantest/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String sayHello() {
-        return "Selam, Furkan!";
+        return "Selam, Furkan! Bu uygulama Docker üzerinde çalışıyor.";
     }
 }
