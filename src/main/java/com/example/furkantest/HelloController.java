301ccbc70957b0ee743effa8bd936f2a0913ec96
furkan <kilic.furkank@gmail.com>
2024-12-31 11:00:57 +0300

    test

--- src/main/java/com/example/furkantest/HelloController.java
+++ src/main/java/com/example/furkantest/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String sayHello() {
-        return "Selam, Furkan! Bu uygulama Docker üzerinde çalışıyor. test2 nameconflict";
+        return "Selam, Furkan! Bu uygulama Docker üzerinde çalışıyor. WEBHOOK TEST";
     }
 }
