c900dd99121cc3b880299ef571cad68b74f6b6a8
furkannkilicc <44247689+furkannkilicc@users.noreply.github.com>
2025-01-02 18:16:23 +0300

    Update HelloController.java

--- src/main/java/com/example/furkantest/HelloController.java
+++ src/main/java/com/example/furkantest/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String sayHello() {
-        return "Selam, Furkan! Bu uygulama Docker üzerinde çalışıyor. Docker in docker";
+        return "Selam, Furkan! Bu uygulama Docker üzerinde çalışıyor. Docker in docker2";
     }
 }
